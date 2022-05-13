9b1405ad059c56f9ce9939a6989df620da357875
MarianaMSA <80396744+MarianaMSA@users.noreply.github.com>
2022-05-13 14:54:28 +0000

    siteYoga

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ document.getElementById("botaoEnviar").addEventListener("click",validaFormulario
 function validaFormulario(){
 
   if(document.getElementById("nome").value != "" &&
+     document.getElementById("telefone").value != "" &&
      document.getElementById("email").value != ""){
 
     alert("Prontinho! você receberá as novidades por e-mail.")
   }else{
-    alert("Por favor, preencha os campos Nome e e-mail.")
+    alert("Por favor, preencha os campos nome, telefone e e-mail.")
   }
 }
 
